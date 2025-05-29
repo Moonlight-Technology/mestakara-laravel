386d23361f74b1e1e022d81478abec75d3f13495
Asep Gumelar <asegume@gmail.com>
2025-05-29 11:34:50 +0700

    clean-clear-code

--- themes/Mytravel/Car/Models/MytravelCarTranslation.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Themes\Mytravel\Car\Models;
-class MytravelCarTranslation extends \Modules\Car\Models\CarTranslation
-{
-    protected $casts = [
-        'faqs'  => 'array',
-        'specifications' => 'array',
-    ];
-}
